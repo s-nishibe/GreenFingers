049ba5539d96f61da12a93aca55949ec685c6842
SakiNishibe <59555340+SakiNishibe@users.noreply.github.com>
2020-04-07 13:30:46 +0900

    [add]view(admins)仮作成

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,7 @@
 class Blog < ApplicationRecord
 	belongs_to :user
+	has_many :blog_comments
+	has_many :stamps
 
 	validates :title, length: {maximum: 100}
 	validates :content, length: {minimum: 2}
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,6 @@
 class Topic < ApplicationRecord
 	belongs_to :user
+	has_many :topic_comments
 
 	validates :title, length: {minimum: 2, maximum: 100}
 end
