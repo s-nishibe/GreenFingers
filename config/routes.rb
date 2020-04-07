1f65db5b3952875508b62f26ce598b139f43baa8
SakiNishibe <59555340+SakiNishibe@users.noreply.github.com>
2020-04-07 09:50:40 +0900

    [add]routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  get 'homes/top' => 'homes#top'
+  get 'homes/about' => 'homes#about'
+
+  resources :users, only: [:index, :show, :edit, :update, :destroy]
+
+  resources :drafts
+    post 'draft/:id/preview' => 'draft#preview', as: 'draft_preview'
+    get 'draft/:id/success' => 'draft#success', as: 'draft_success'
+
+  resources :blogs, only: [:create, :index, :show, :edit, :update, :destroy] do
+    resource :blog_comments, only: [:new, :create, :edit, :update, :destroy]
+  end
+
+  resources :stamps, only: [:create, :destroy]
+
+  resources :topics, only: [:new, :create, :index,  :edit, :show, :update] do
+    resources :topic_comments, only: [:new, :create, :destroy]
+  end
 end
+
