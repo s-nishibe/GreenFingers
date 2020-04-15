4f4caf4c15d7ddca097d32f8b063a34432e13f7a
SakiNishibe <59555340+SakiNishibe@users.noreply.github.com>
2020-04-15 22:09:51 +0900

    [update]Plant edit(ajax)

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -24,20 +24,26 @@ def create
     @plant = Plant.find(params[:blog][:plant])
     @blog.plant_name = @plant.name
     @blog.plant_kind = @plant.kind
-    binding.pry
-    @blog.save
-    redirect_to blog_path(@blog)
-    flash[:success] = 'ブログを下書き保存しました。'
+    if @blog.save
+      redirect_to blog_path(@blog)
+      flash[:success] = 'ブログを下書き保存しました。'
+    else
+      render :new
+      flash[:danger] = 'ブログを保存できません。空欄になっている箇所はありませんか？'
+    end
   else params[:blog_btn]
     @blog = current_user.blogs.build(blog_params)
     @plant = Plant.find(params[:blog][:plant])
     @blog.plant_name = @plant.name
     @blog.plant_kind = @plant.kind
     @blog.status = true
-    binding.pry
-    @blog.save
-    redirect_to blog_path(@blog)
-    flash[:success] = 'blog created!'
+    if @blog.save
+      redirect_to blog_path(@blog)
+      flash[:success] = 'blog created!'
+    else
+      render :new
+      flash[:danger] = 'ブログを保存できません。空欄になっている箇所はありませんか？'
+    end
   end
 end
 
@@ -49,16 +55,17 @@ def index
   if params[:tag_name]
     @tag = params[:tag_name]
     @blogs = Blog.tagged_with("#{params[:tag_name]}").order(updated_at: :DESC)
-  elsif params[:from] == 'sidebar'
+  elsif params[:page] == 'user_blogs'
     @user = User.find(params[:id])
     @blogs = @user.blogs.order(updated_At: :DESC)
-  else params[:from] == 'header'
+  else params[:page] == 'all_blogs'
     @blogs = Blog.order(updated_at: :DESC)
   end
 end
 
 def edit
   @blog = Blog.find(params[:id])
+  render :edit
 end
 
 def show
@@ -70,28 +77,42 @@ def show
 end
 
 def update
-  if params[:draft_btn]
-  	@draft = Draft.new
-  	@draft.user_id = current_user.id
-  	@draft.eyecatch_img = @blog.eyecatch_img
-  	@draft.title = @blog.title
-  	@draft.tag_list = @blog.tag_list
-  	@draft.content = @blog.content
-  	if @draft.save(save_params)
-  	  @blog.destroy
-  	  redirect_to drafts_path
-  	  flash[:info] = '記事を下書きに戻しました。'
+  if params[:preview_btn]
+    @blog = Blog.new
+    @plant = Plant.find(params[:blog][:plant])
+    @blog.title = params[:blog][:title]
+    @blog.eyecatch_img = params[:blog][:eyecatch_img]
+    @blog.weather = params[:blog][:weather]
+    @blog.temperature = params[:blog][:tempearture]
+    @blog.plant_name = @plant.name
+    @blog.plant_kind = @plant.kind
+    @blog.tag_list = params[:blog][:tag_list]
+    @blog.content = params[:blog][:content]
+    render :preview
+  elsif params[:draft_btn]
+    @blog = Blog.find(params[:id])
+    @plant = Plant.find(params[:blog][:plant])
+    @blog.plant_name = @plant.name
+    @blog.plant_kind = @plant.kind
+    if @blog.save
+      redirect_to blog_path(@blog)
+      flash[:success] = 'ブログを下書き保存しました。'
     else
-      render :edit
-      flash[:danger] = '記事を下書きに戻せません。タイトルや本文の文字数は適切ですか？'
+      render :new
+      flash[:danger] = 'ブログを保存できません。空欄になっている箇所はありませんか？'
     end
-  elsif params[:blog_btn]
-    if @blog.update(save_params)
-    redirect_to blog_path(@blog)
-    flash[:success] = 'ブログが更新されました！'
+  else params[:blog_btn]
+    @blog = Blog.find(params[:id])
+    @plant = Plant.find(params[:blog][:plant])
+    @blog.plant_name = @plant.name
+    @blog.plant_kind = @plant.kind
+    @blog.status = true
+    if @blog.save
+      redirect_to blog_path(@blog)
+      flash[:success] = 'blog created!'
     else
-      render :edit
-      flash[:danger] = '記事を更新できません。タイトルや本文の文字数は適切ですか？'
+      render :new
+      flash[:danger] = 'ブログを保存できません。空欄になっている箇所はありませんか？'
     end
   end
 end
--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -5,10 +5,8 @@ class PlantsController < ApplicationController
   	@plant = @user.plants.build(plant_params)
     if @plant.save
       render :index
-      flash[:success] = '新しい花が登録されました！'
     else
       redirect_back(fallback_location: root_path)
-      flash[:danger] = '花の登録に失敗しました。'
     end
   end
 
@@ -19,13 +17,13 @@ class PlantsController < ApplicationController
   def update
     @plant = Plant.find(params[:id])
     @plant.update(plant_params)
-    redirect_to user_path(@user)
+    render :index
   end
 
   def destroy
     @plant = Plant.find(params[:id])
     @plant.destroy
-    redirect_to user_path(@user)
+    render :index
   end
 
   private
@@ -34,6 +32,6 @@ class PlantsController < ApplicationController
   end
 
   def plant_params
-  	params.require(:plant).permit(:user_id, :name, :name, :memo)
+  	params.require(:plant).permit(:user_id, :name, :kind, :memo)
   end
 end
