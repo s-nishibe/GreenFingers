661a8680ec653a43f18ba25857f927e7f6b5bba6
SakiNishibe <59555340+SakiNishibe@users.noreply.github.com>
2020-04-15 09:16:19 +0900

    [update]ruby2.7.1

--- db/schema.rb
+++ db/schema.rb
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 2020_04_14_005242) do
   create_table "blogs", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "title", default: "", null: false
-    t.text "content", default: "", null: false
+    t.text "content", null: false
     t.string "eyecatch_img_id", default: "", null: false
     t.string "plant_name", default: "", null: false
     t.integer "plant_type", default: 1, null: false
@@ -64,7 +64,7 @@ ActiveRecord::Schema.define(version: 2020_04_14_005242) do
   create_table "drafts", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "title", default: "", null: false
-    t.text "content", default: "", null: false
+    t.text "content", null: false
     t.string "eyecatch_img_id", default: "", null: false
     t.string "plant_name", default: "", null: false
     t.integer "plant_type", default: 1, null: false
@@ -79,7 +79,7 @@ ActiveRecord::Schema.define(version: 2020_04_14_005242) do
     t.integer "user_id", null: false
     t.string "name"
     t.integer "type", default: 0, null: false
-    t.text "memo", default: "", null: false
+    t.text "memo", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -110,15 +110,8 @@ ActiveRecord::Schema.define(version: 2020_04_14_005242) do
     t.integer "tagger_id"
     t.string "context", limit: 128
     t.datetime "created_at"
-    t.index ["context"], name: "index_taggings_on_context"
-    t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
     t.index ["tag_id"], name: "index_taggings_on_tag_id"
     t.index ["taggable_id", "taggable_type", "context"], name: "taggings_taggable_context_idx"
-    t.index ["taggable_id", "taggable_type", "tagger_id", "context"], name: "taggings_idy"
-    t.index ["taggable_id"], name: "index_taggings_on_taggable_id"
-    t.index ["taggable_type"], name: "index_taggings_on_taggable_type"
-    t.index ["tagger_id", "tagger_type"], name: "index_taggings_on_tagger_id_and_tagger_type"
-    t.index ["tagger_id"], name: "index_taggings_on_tagger_id"
   end
 
   create_table "tags", force: :cascade do |t|
@@ -126,7 +119,6 @@ ActiveRecord::Schema.define(version: 2020_04_14_005242) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "taggings_count", default: 0
-    t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
   create_table "topic_comments", force: :cascade do |t|
