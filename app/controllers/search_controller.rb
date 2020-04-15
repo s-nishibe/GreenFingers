49d536edd4f0dd2b2d72907459de0fcfd3d05b89
SakiNishibe <59555340+SakiNishibe@users.noreply.github.com>
2020-04-16 08:48:37 +0900

    [add]search(controller)

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,21 @@
+class SearchController < ApplicationController
+  def search
+  	@user = User.new
+  	@blog = Blog.new
+  	@topic = Topic.new
+  	@model = params[:search][:model]
+  	@word = params[:search][:word]
+  	@datas = search_for(@model, @content)
+  end
+
+  private
+  def search_for(model, word)
+  	if model == 'user'
+  	  User.where("name LIKE ?", "%#{word}%")
+  	elsif model == 'blog'
+  	  Blog.where("title || content LIKE ?", "%#{word}%")
+  	else model == 'topic'
+  	  Topic.where("title LIKE ?", "%#{word}%")
+    end
+  end
+end
