9fe80a576dcce2507925b6d0b00575c513606c5f
SakiNishibe <59555340+SakiNishibe@users.noreply.github.com>
2020-04-15 18:07:36 +0900

    [update]follow機能ajax化

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,11 +4,9 @@ class RelationshipsController < ApplicationController
     @user = User.find(params[:following_id])
     following = current_user.follow(@user)
     if following.save
-      flash[:success] = 'ユーザーをフォローしました！'
-      redirect_back(fallback_location: root_path)
+      render :index
     else
       flash.now[:alert] = 'ユーザーのフォローに失敗しました。'
-      redirect_back(fallback_location: root_path)
     end
   end
 
@@ -16,11 +14,9 @@ class RelationshipsController < ApplicationController
     @user = User.find(params[:id])
     following = current_user.unfollow(@user)
     if following.destroy
-      flash[:info] = 'フォローを解除しました。'
-      redirect_back(fallback_location: root_path)
+      render :index
     else
       flash.now[:alert] = 'フォロー解除に失敗しました。'
-      redirect_back(fallback_location: root_path)
     end
   end
 
