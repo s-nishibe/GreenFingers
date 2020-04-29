0fd90b21774ffcade415e91d4bc2d566618e0d84
SakiNishibe <59555340+SakiNishibe@users.noreply.github.com>
2020-04-29 09:43:37 +0900

    [fix]plant,blog dependent: :destroy

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,6 +1,6 @@
 class Plant < ApplicationRecord
   belongs_to :user
-  has_many :blogs
+  has_many :blogs, dependent: :destroy
 
   validates :name, length: {minimum: 1, maximum: 20}
   validates :memo, length: { maximum: 200 }
