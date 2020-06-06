a1d424d4842cc1146524243dd0ed70d2282fdba0
SakiNishibe <59555340+SakiNishibe@users.noreply.github.com>
2020-06-06 15:58:55 +0900

    [update]admin/topic

--- app/admin/topics.rb
+++ app/admin/topics.rb
@@ -14,5 +14,42 @@ ActiveAdmin.register Topic do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
+
+    # 入力フォーム
+  form do |f|
+    f.inputs do
+      f.input :user
+      f.input :title
+      f.input :category, as: :select, collection: {'育て方全般' => 0, '剪定' => 1, '土作り' => 2, '肥料' => 3, '病害虫' => 4, 'その他' => 5}
+      f.input :status, as: :select, :collection: {'解決済み' => true, '未解決' => :false}
+    end
+    f.actions
+  end
+
+  # 一覧画面
+  index do
+    selectable_column
+    id_column
+    column :user
+    column :title
+    column :status
+    column 'comments' do |topic|
+      topic.topic_comments.count
+    end
+    actions
+  end
+
+  # 詳細画面
+  show do |blog|
+    attributes_table(*blog.class.columns.collect { |column| column.name.to_sym })
+    panel "コメント一覧" do
+      table_for topic.topic_comments do
+        column :user
+        column :comment
+        column :updated_at
+      end
+    end
+    active_admin_comments
+  end
+
 end
